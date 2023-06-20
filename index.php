2b8d65361fd6e5b83cbdeff1de91d043d32f6b76
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-06-20 14:56:54 +0200

    add: animalSpecies

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-2</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
+
+<?php
+
+class animalSpecies
+{
+    public $species;
+    public $size;
+    public $type;
+
+    public function __construct($species, $size, $type)
+    {
+        $this->species = $species;
+        $this->size = $size;
+        $this->type = $type;
+    }
+}
+
+$Dog = new animalSpecies('Cane', 'Grande', 'Bulldog');
+
+var_dump($dog);
+
+
+$Cat = new animalSpecies('Catto', 'Piccola', 'Persiano');
+
+var_dump($Cat);
